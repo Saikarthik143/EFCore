c720ebdeb659c4bc51190ef91fcb793fa6ffe021
Saikarthik143 <saikarthik8645@gmail.com>
2020-02-20 18:18:46 +0530

    feb20

---
+++ Microservices/ItemService/Controllers/ItemController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ItemService.Models;
+using ItemService.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemRepo _repo;
+        public ItemController(IItemRepo repo)
+        {
+            _repo = repo;
+        }
+        //get all items
+        [HttpGet]
+        [Route("GetItems")]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_repo.GetAllItems());
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(string id)
+        {
+            try
+            {
+                return Ok(_repo.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Post(Items items)
+        {
+            try
+            {
+                _repo.AddItem(items);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpPut]
+        [Route("Update")]
+        public IActionResult Put(Items items)
+        {
+            try
+            {
+                _repo.UpdateItem(items);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpDelete]
+        [Route("Delete")]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                _repo.DeleteItem(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
---
+++ Microservices/ItemService/Repositories/ItemRepository.cs
@@ -0,0 +1,52 @@
+﻿using ItemService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemService.Repositories
+{
+    public class ItemRepository : IItemRepo
+    {
+        private readonly ShopDBContext _context;
+        public ItemRepository(  ShopDBContext context)
+        {
+            this._context = context;
+      
+        }
+        public void AddItem(Items obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+            
+        }
+
+        public void DeleteItem(string id)
+        {
+            Items items = _context.Items.Find(id);
+            _context.Remove(items);
+            _context.SaveChanges();
+            throw new NotImplementedException();
+        }
+
+        public List<Items> GetAllItems()
+        {
+            return _context.Items.ToList();
+            throw new NotImplementedException();
+        }
+
+        public Items GetById(string id)
+        {
+            return _context.Items.Find(id);
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Items obj)
+        {
+            _context.Items.Update(obj);
+            _context.SaveChanges();
+            
+            throw new NotImplementedException();
+        }
+    }
+}
